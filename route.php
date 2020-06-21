ea4f88bc49974a4a8c45dbf3f94f8371eb7599dc
Danny <danny@dkamps18.net>
2020-06-21 11:55:54 +0200

    added route

---
+++ route.php
@@ -0,0 +1,13 @@
+<?php
+    $route = explode('/', explode('?', $_SERVER['REQUEST_URI'], 2)[0]);
+
+    // Example: http://localhost/test/route.php
+    // array(3) {
+    //     [0]=>
+    //     string(0) ""
+    //     [1]=>
+    //     string(4) "test"
+    //     [2]=>
+    //     string(9) "route.php"
+    // }
+?>
