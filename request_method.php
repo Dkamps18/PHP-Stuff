4c10b100de13b17612926084b2cc38fdec5c5615
Danny <danny@dkamps18.net>
2020-09-02 09:22:47 +0200

    Added http request method stuff

---
+++ request_method.php
@@ -0,0 +1,24 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'HEAD') {
+
+    }
